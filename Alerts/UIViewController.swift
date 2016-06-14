e4857161aeb4270bfff2431c863b425fff70f975
Matthew Di Pasquale <liveloveprosper@gmail.com>
2016-06-14 10:25:24 -0400

    Add UIHelper

--- Alerts/UIViewController.swift
+++ Alerts/UIViewController.swift
@@ -1,19 +1,12 @@
 import UIKit
-
-public func topmostViewController() -> UIViewController {
-    var topmostViewController = UIApplication.sharedApplication().delegate!.window!!.rootViewController!
-    while let presentedViewController = topmostViewController.presentedViewController {
-        topmostViewController = presentedViewController
-    }
-    return topmostViewController
-}
+import UIHelper
 
 public func alert(title title: String?, message: String?, handler: ((UIAlertAction) -> Void)? = nil) {
     let alert = UIAlertController.OKAlert(title: title, message: message, handler: handler)
-    topmostViewController().presentViewController(alert, animated: true, completion: nil)
+    UIApplication.auh_topmostViewController.presentViewController(alert, animated: true, completion: nil)
 }
 
 public func confirm(title title: String?, message: String?, handler: (UIAlertAction) -> Void) {
     let alert = UIAlertController.yesNoAlert(title: title, message: message, handler: handler)
-    topmostViewController().presentViewController(alert, animated: true, completion: nil)
+    UIApplication.auh_topmostViewController.presentViewController(alert, animated: true, completion: nil)
 }
