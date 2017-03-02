9420b8b35e8f3e2fe2d5422ef87c395516918b3e
Matthew Di Pasquale <liveloveprosper@gmail.com>
2017-03-02 14:26:42 -0500

    Remove present's optional completion argument
    Co-Authored-By: Matt Packer <matthewrpacker@gmail.com>

--- Alerts/UIViewController.swift
+++ Alerts/UIViewController.swift
@@ -3,10 +3,10 @@ import UIHelper
 
 public func alertTitle(_ title: String?, message: String?, handler: ((UIAlertAction) -> Swift.Void)? = nil) {
     let alert = UIAlertController.OKAlert(title: title, message: message, handler: handler)
-    UIApplication.auh_topmostViewController.present(alert, animated: true, completion: nil)
+    UIApplication.auh_topmostViewController.present(alert, animated: true)
 }
 
 public func confirmTitle(_ title: String?, message: String?, handler: @escaping (UIAlertAction) -> Swift.Void) {
     let alert = UIAlertController.yesNoAlert(title: title, message: message, handler: handler)
-    UIApplication.auh_topmostViewController.present(alert, animated: true, completion: nil)
+    UIApplication.auh_topmostViewController.present(alert, animated: true)
 }
